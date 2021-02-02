cfc56fb555734a522db8d4075e4844b626603729
Kjetil Golid <kjetil.golid@gmail.com>
2021-02-02 19:47:57 +0100

    update rollup packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default [
   {
